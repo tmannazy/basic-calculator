6256b38782e5651888b874648ea4e423d621a7a2
tmannazy <e.anene@hotmail.com>
2021-01-19 20:46:43 +0100

    Return the solution of add values

--- script.js
+++ script.js
@@ -8,57 +8,49 @@ divide = (a, b) => a / b;
 
 operate = (operator) => {
     if (operator === '+') {
-        add(buttonValue, buttonValueTwo);
+        return add(+buttonValue.join(''), +buttonValueTwo.join(''));
     };
 };
 
 const buttons = document.querySelector('.calc-body');
 const displayScreen = document.querySelector('.calc-display');
-let buttonValue;
-let buttonValueTwo;
+const buttonValue = [];
+let buttonValueTwo  = [];
 let operatorSign;
 
 calculation = () => {
     buttons.addEventListener('click', (e) => {
         if (e.target !== e.currentTarget) {
-            if (e.target.id !== 'equals' &&
+            if (e.target.id === 'plus' ||
+                e.target.id === 'minus' ||
+                e.target.id === 'multiply' ||
+                e.target.id === 'divide') {
+                displayScreen.innerHTML += ' ' + e.target.textContent + ' ';
+                operatorSign = e.target.textContent;
+            }
+            else if (buttonValue.length !== 0 &&
+                e.target.id !== 'equals' &&
+                e.target.id !== 'clear' &&
+                e.target.id !== 'plus' &&
+                e.target.id !== 'minus' &&
+                e.target.id !== 'multiply' &&
+                e.target.id !== 'divide' ) {
+                displayScreen.innerHTML += e.target.textContent;
+                buttonValueTwo.push(e.target.textContent);
+            }
+            else if (e.target.id !== 'equals' &&
                 e.target.id !== 'clear' &&
                 e.target.id !== 'plus' &&
                 e.target.id !== 'minus' &&
                 e.target.id !== 'multiply' &&
                 e.target.id !== 'divide') {
                 displayScreen.innerHTML += e.target.textContent;
-                return buttonValue = +e.target.textContent;
-                // return buttonValue;
+                buttonValue.push(e.target.textContent);
             }
-            else if (e.target.id === 'plus' ||
-                e.target.id === 'minus' ||
-                e.target.id === 'multiply' ||
-                e.target.id === 'divide') {
-                if (e.target.id !== 'equals' &&
-                    e.target.id !== 'clear') {
-                        displayScreen.innerHTML += ' ' + e.target.textContent + ' ';
-                        operatorSign = e.target.textContent;
-                    }
-                else {
-                    return buttonValueTwo = +e.target.textContent;
-                    }
-            }
-            // else target.id !== 'plus' &&
-            //     e.target.id !== 'minus' &&
-            //     e.target.id !== 'multiply' &&
-            //     e.target.id !== 'divide') {
-            // }
             else if (e.target.id === 'equals') {
                 displayScreen.innerHTML = operate(operatorSign);
             }
-        }
-            // buttonValue = +displayScreen.textContent;
-            // return buttonValue;
-         e.stopPropagation();
-        // if (e.target === +) {
-
-        // }
+        } e.stopPropagation();
     });
 }
 
