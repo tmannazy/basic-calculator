03ec164309d962004060dff80229a56b99a206bf
tmannazy <e.anene@hotmail.com>
2021-01-19 11:31:35 +0100

    Fix buttons for equals and clear not to display

--- script.js
+++ script.js
@@ -6,21 +6,36 @@ multiply = (a, b) => a * b;
 
 divide = (a, b) => a / b;
 
-operate = (operator, a, b) => {
+operate = (operator, operation) => {
 
 };
 
 const buttons = document.querySelector('.calc-body');
 const displayScreen = document.querySelector('.calc-display');
 let buttonValue;
+let operatorSign;
+let decision = ['=', 'C'].map(item => item);
 
-const value = buttons.addEventListener('click', (e) => {
-    if (e.target !== e.currentTarget) {
-        displayScreen.innerHTML = e.target.textContent;
-        buttonValue = +displayScreen.textContent;
-        return buttonValue;
-    } e.stopPropagation();
-});
+calculation = () => {
+    buttons.addEventListener('click', (e) => {
+        if (e.target !== e.currentTarget) {
+            if (e.target.id === 'equals' || e.target.id  === 'clear') {
+                displayScreen.innerHTML = '';
+            }else {
+                displayScreen.innerHTML = e.target.textContent;
+            }
+        }
+            // buttonValue = +displayScreen.textContent;
+            // return buttonValue;
+         e.stopPropagation();
+        // if (e.target === +) {
+
+        // }
+    });
+}
+
+
+calculation();
 
 
 
