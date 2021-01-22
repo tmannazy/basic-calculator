fb40f630e6647fa3a2fe7b3aabeabd0ea6374cd8
tmannazy <e.anene@hotmail.com>
2021-01-22 16:10:17 +0100

    Solve calculations of one digit operands

--- script.js
+++ script.js
@@ -1,66 +1,48 @@
-const add = (a, b) =>  a + b;
-
-const subtract = (a, b) => a - b;
-
-const multiply = (a, b) => a * b;
-
-const divide = (a, b) => a / b;
-
-const operate = (operator) => {
-    if (operator === '+') {
-        return add(+buttonValue.join(''), +buttonValueTwo.join(''));
-    };
-};
-
-const displayScreen = (scr) => {
-    screen = document.querySelector('.calc-display');
-    return screen.innerHTML += scr;
-}
-
 const buttons = document.querySelector('.calc-body');
+const screen = document.querySelector('.calc-display');
 const leftOperand = [];
 const rightOperand = [];
 let operatorSign;
 
-// calculation = () => {
-//     buttons.addEventListener('click', (e) => {
-//         if (e.target !== e.currentTarget) {
-//             if (e.target.id === 'plus' ||
-//                 e.target.id === 'minus' ||
-//                 e.target.id === 'multiply' ||
-//                 e.target.id === 'divide') {
-//                 displayScreen.innerHTML += ' ' + e.target.textContent + ' ';
-//                 operatorSign = e.target.textContent;
-//             }
-//             else if (buttonValue.length !== 0 &&
-//                 e.target.id !== 'equals' &&
-//                 e.target.id !== 'clear' &&
-//                 e.target.id !== 'plus' &&
-//                 e.target.id !== 'minus' &&
-//                 e.target.id !== 'multiply' &&
-//                 e.target.id !== 'divide') {
-//                 displayScreen.innerHTML += e.target.textContent;
-//                 buttonValueTwo.push(e.target.textContent);
-//             }
-//             else if (e.target.id !== 'equals' &&
-//                 e.target.id !== 'clear' &&
-//                 e.target.id !== 'plus' &&
-//                 e.target.id !== 'minus' &&
-//                 e.target.id !== 'multiply' &&
-//                 e.target.id !== 'divide') {
-//                 displayScreen.innerHTML += e.target.textContent;
-//                 buttonValue.push(e.target.textContent);
-//             }
-//             else if (e.target.id === 'equals') {
-//                 displayScreen.innerHTML = operate(operatorSign);
-//             }
-//             else if (e.target.id === 'clear') {
-//                 displayScreen.innerHTML = ' ';
-//                 buttonValue = buttonValueTwo = [];
-//             }
-//         } e.stopPropagation();
-//     });
-// }
+const add = (a, b) =>  a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
+const operate = (operator) => {
+    if (operator === '+') {
+        return screen = displayScreen(add(+leftOperand, +rightOperand));
+    }
+    else if (operator === '-') {
+        return screen = displayScreen(subtract(+leftOperand, +rightOperand));
+    }
+    else if (operator === 'x') {
+        return screen += displayScreen(multiply(+leftOperand, +rightOperand));
+    }
+    else {
+        return screen += displayScreen(divide(+leftOperand, +rightOperand));
+    }
+};
+
+const isOperator = (e) => {
+    if (e === 'plus') {
+        return e;
+    }
+    else if (e === 'minus') {
+        return e;
+    }
+    else if (e === 'multiply') {
+        return e;
+    }
+    else if (e === 'divide') {
+        return e;
+    }
+};
+
+const displayScreen = (scr) => {
+    console.log(screen.innerHTML = scr);
+    screen.innerHTML = scr;
+}
 
 const calculation = () => {
     buttons.addEventListener('click', selectButton);
@@ -70,47 +52,31 @@ const selectButton = (e) => {
     if (e.target !== e.currentTarget) {
         if (e.target.id === isOperator(e.target.id)) {
             operatorSign = e.target.textContent;
-            displayScreen(operatorSign);
-            console.log(displayScreen);
+            screen.innerHTML += ' ' + operatorSign + ' ';
+        }
+        else if (e.target.id === 'equals') {
+            if (!operatorSign){
+                screen.innerHTML = 0;
+            }
+            else if (operatorSign) {
+                operate(operatorSign);
+            }
         }
         else if (e.target.id !== isOperator(e.target.id)) {
             if (operatorSign) {
                 rightOperand.push(e.target.textContent);
+                displayScreen(rightOperand.join(''));
             }
             else {
                 leftOperand.push(e.target.textContent);
-                console.log(leftOperand);
+                displayScreen(leftOperand.join(''));
             }
         }
-    }
+
+    } e.stopPropagation;
 };
-
-
-const isOperator = (e) => {
-    if (e === 'plus') {
-        // add(leftOperand, rightOperand);
-        return e;
-    }
-    else if (e === 'minus') {
-        return e;
-    }
-    else if (e === 'multiply') {
-        // multiply(leftOperand, rightOperand);
-        return e;
-    }
-    else if (e === 'divide') {
-        // divide(leftOperand, rightOperand);
-        return e;
-    }
-};
-
-
-
-
-
 
 calculation();
-// console.log(calculation());
 
 
 
